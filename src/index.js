0facaabccbf3c380fdded51fd4a6ad363b0b9576
CuppaKe <turnman87@gmail.com>
2018-09-20 13:35:20 +0300

    add function for checking sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str.length % 2 > 0) return false;
+
+    let arr = bracketsConfig.map(function(arrsmall){
+		return arrsmall.join("");
+	});
+
+    for(let i = 0; i<arr.length; i++){
+
+        if( !str.includes(arr[i])){
+
+	     return false;
+	    } else return true;
+
+    }
+ 
 }
