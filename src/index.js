1b899682ce59195120271f6031441f765fbf1c0e
CuppaKe <turnman87@gmail.com>
2018-09-28 19:09:59 +0300

    add commentaries

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+/**
+ * Function checks brackets sequence on containing given config
+ * @param {string} str - brackets sequence
+ * @param {array} bracketsConfig - the array of pairs open-closed brackets
+ * @return {boolean} correct brackets sequence or not
+ */
 module.exports = function check(str, bracketsConfig) {
-  if (str.length % 2 > 0) return false;
+  if (str.length % 2 !== 0) return false;
 
   let string = str;
   const arr = bracketsConfig.map(smallarr => smallarr.join(''));
@@ -9,7 +15,10 @@ module.exports = function check(str, bracketsConfig) {
       if (string.includes(arr[i])) {
         string = string.replace(arr[i], '');
       }
-    } if (arr.every(brackets => string.includes(brackets) === false)) break;
+    }
+
+    if (arr.every(brackets => string.includes(brackets) === false)) break;
   }
+
   return string.length === 0;
 };
